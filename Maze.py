073a32d5e761c6f6db11a81331896c9ce33b135b
Cyrannosaurus <70718730+Cyrannosaurus@users.noreply.github.com>
2021-01-20 15:55:42 -0600

    separation of maze and maze generator
    as of now they do not work in tandem

--- Maze.py
+++ Maze.py
@@ -1,66 +1,114 @@
-import random as rand, pickle 
-from Agent import Agent
+import pickle
 
 class Maze():
-    ## generate multiple shapes
-    def __init__(self):
-        self.features = dict({'empty':0,
-                        'wall':1,
-                        'honey':2,
-                        'ice':3
-        }) 
+    def __init__(self, filename):
+        # Read file
+        with open(filename, 'r') as f:
+            contents = f.read()
+        
+        if contents.count('A') != 1:
+            raise Exception("Maze needs a single startpoint")
+        if contents.count('B') != 1:
+            raise Exception("Maze needs a single endpoint")
 
-        self.locations = dict({'start':-1, 'goal':-2})
-    def generate_random(self) -> list:
-        """ Generate a maze with random size, start state and goal state """
-        size = (rand.randint(20,100), rand.randint(20,100))
-        start = (rand.randint(0, size[0]-1), rand.randint(0, size[1]-1))
-        goal = (rand.randint(0, size[0]-1), rand.randint(0, size[1]-1))
-        while goal == start:
-            goal = (rand.randint(0, size[0]-1), rand.randint(0, size[1]-1))
-        return self.generate(size, start, goal)
+        contents = contents.splitlines()
+        # Record height and width of maze
+        self.height = len(contents)
+        self.width = max(len(line) for line in contents)
 
-    def generate(self, size:tuple, start_state:tuple, goal_state:tuple) -> list:
-        """ Generate a maze with given dimensions and start/goal states """
-        maze = list()
-        untouchables = dict({start_state:1, goal_state:1})
-        for row in range(size[0]):
-            column = list()
-            for col in range(size[1]):
-                if (row,col) == start_state:
-                    column.append(self.locations['start'])
-                elif (row,col) == goal_state:
-                    column.append(self.locations['goal'])
+        # Add context
+        self.walls = [] # 2D bool array
+        for r in range(self.height):
+            row = []
+            for c in range(self.width):
+                # Try block as not all mazes will be rectangular
+                try:
+                    if contents[r][c] == '#':
+                        row.append(True)
+                    elif contents[r][c] == ' ':
+                        row.append(False)
+                    elif contents[r][c] == 'A':
+                        self.start = (r,c)
+                        row.append(False)
+                    elif contents[r][c] == 'B':
+                        self.goal = (r,c)
+                        row.append(False)
+                    else:
+                        row.append(True)
+                except IndexError:
+                    row.append(True)
+            self.walls.append(row)
+        self.solution = None
+
+        self.horizontal_edges = set([0, self.height])
+        self.vertical_edges = set([0, self.width])
+
+        
+    def print(self):
+        solution = self.solution[1] if self.solution is not None else None
+        for row in range(self.height):
+            for col in range(self.width):
+                if (row,col) == self.start:
+                    print('A', end='')
+                elif (row,col) == self.goal:
+                    print('B', end='')
+                elif self.walls[row][col] == True:
+                    print('#', end='')
+                elif solution is not None and (row,col) in solution:
+                    print('*', end='')
                 else:
-                    column.append(rand.choice(list(self.features.values())))
-            maze.append(column)
+                    print(' ', end='')
+            print()
+        print()
 
-        while Agent.BFS(maze, start_state, goal_value=self.locations['goal'], wall_value=self.features['wall'])[0] == False:
-            location1 = rand.randint(0,size[0]-1), rand.randint(0, size[1]-1)
-            location2 = rand.randint(0,size[0]-1), rand.randint(0, size[1]-1)
+        
+    def neighbors(self, state):
+        row, col = state
+        # All possible actions
+        candidates = [
+            ('up', (row-1, col)),
+            ('down', (row+1, col)),
+            ('left', (row, col-1)),
+            ('right', (row, col+1)),
+        ]
+        # Validate actions
+        possible = []
+        for action, (row, col) in candidates:
+            try:
+                if not self.walls[row][col]:
+                    possible.append((action, (row,col)))
+            except IndexError:
+                continue
+        return possible
 
-            # It is still possible for l1 to = l2, due to later reassignment
-            # The chancees were already low enough that I was questioning this next while loop anyhow
-            while location1 == location2:
-                location2 = rand.randint(0,size[0]-1), rand.randint(0, size[1]-1)
+    # Test which neighbor function is faster
+    # def neighbors(self, state:Tuple[int,int]) -> list:
+    #     neighbors = list()
 
-            while location1 in untouchables:
-                location1 = rand.randint(0,size[0]-1), rand.randint(0, size[1]-1)
+    #     r = state[0]
+    #     c = state[1]
 
-            while location2 in untouchables:
-                location2 = rand.randint(0,size[0]-1), rand.randint(0, size[1]-1)
+    #     if r in self.horizontal_edges:
+    #         if r == 0:
+    #             neighbors.append((1, c))
+    #         else:
+    #             neighbors.append((self.height-1, c))
+    #     else:
+    #         neighbors.append((r-1, c))
+    #         neighbors.append((r+1, c))
 
-            maze[location1[0]][location1[1]], maze[location2[0]][location2[1]] = maze[location2[0]][location2[1]], maze[location1[0]][location1[1]]
-        return maze
+    #     if c in self.vertical_edges:
+    #         if c == 0:
+    #             neighbors.append((r, 1))
+    #         else:
+    #             neighbors.append((r, self.width-1))
+    #     else:
+    #         neighbors.append((r, c-1))
+    #         neighbors.append((r, c+1))
 
+    #     return neighbors
 
-    def save(maze:list, filename:str) -> None:
-        """ Not implemented """
-        with open(filename, 'w') as f:
-            for col in maze:
-                pass
-
-    def psave(maze:list, filename:str):
-        """ Serialize with pickle. File must be *.p """
+    def serialize(self, filename:str):
+        """ Serialize with pickle. filename must be *.p """
         with open(filename, 'wb') as fp:
-            pickle.dump(filename, fp)
+            pickle.dump(self, fp)
